e4cdb5c762f06fa2da9613ea6d6d20c7dbdd0774
David Wright <davidwright7793@gmail.com>
2024-05-14 16:27:56 -0400

    added router outlet

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,33 @@ import About from './components/About.jsx'
 import Contact from './components/Contact.jsx'
 import Portfolio from './components/Portfolio.jsx'
 import Resume from './components/Resume.jsx'
+import { createBrowserRouter, RouterProvider, } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    //errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: '/portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+      {
+        path: '/resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
