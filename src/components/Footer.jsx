dd2f441ebf9154fca03f24dd1aeee0959893708d
David Wright <davidwright7793@gmail.com>
2024-05-15 13:59:09 -0400

    added icons with links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
+import githubLogo from "../assets/github_logo.png"
+import linkedinLogo from "../assets/linkedin_logo.png"
+import twitterxLogo from "../assets/twitterx_logo.png"
 function Footer(){
     return(
         <div>
-            <h1>GitHub: https://github.com/MrOahu</h1>
-            <h1>LinkedIn: https://www.linkedin.com/in/david-wright-305/</h1>
-            <h1>X/Twitter: https://twitter.com/MetalheadMystic</h1>
+            <a href="https://github.com/MrOahu"><img className="footer-logo" src={githubLogo} /></a>
+            <a href="https://www.linkedin.com/in/david-wright-305/"><img className="footer-logo" src={linkedinLogo} /></a>
+            <a href="https://twitter.com/MetalheadMystic"><img className="footer-logo" src={twitterxLogo} /></a>
+            {/* <h1>LinkedIn: https://www.linkedin.com/in/david-wright-305/</h1>
+            <h1>X/Twitter: https://twitter.com/MetalheadMystic</h1> */}
         </div>
     )
 }
