a3c644e7b16c3c1a890adddfff41fab22760399c
David Wright <davidwright7793@gmail.com>
2024-05-16 17:39:15 -0400

    added project card

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,33 @@
 
 function Portfolio(){
+    const myProjects = [
+        {
+            title: "Project 1",
+            imageUrl: "url to image",
+            githubUrl: "http://github.com",
+            liveUrl: "http://google.com"
+        },
+        {
+            title: "Project 2",
+            imageUrl: "url to image",
+            githubUrl: "http://github.com",
+            liveUrl: "http://google.com"
+        },
+        {
+            title: "Project 3",
+            imageUrl: "url to image",
+            githubUrl: "http://github.com",
+            liveUrl: "http://google.com"
+        },
+        {
+            title: "Project 4",
+            imageUrl: "url to image",
+            githubUrl: "http://github.com",
+            liveUrl: "http://google.com"
+        },
+    ]
+
+
     return(
         <div>
             <h1>Here are some of David Wright's projects in React.</h1>
